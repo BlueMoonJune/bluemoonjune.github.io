5f86b211c77c1897b4f3db2d3550029bdfe512cd
BlueMoonJune <45300174+BlueMoonJune@users.noreply.github.com>
2024-01-24 09:59:53 -0800

    change z?

--- main.js
+++ main.js
@@ -15,4 +15,4 @@ const material = new THREE.MeshBasicMaterial( {color: 0x00ffff} );
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = -5;
